8510063eb48ad3b5523518ba644249e5ff8109ce
Alan Varela <alanvareli@gmail.com>
2026-05-11 13:27:21 -0600

    fix: schedules

--- src/app/schedules/schedules.controller.ts
+++ src/app/schedules/schedules.controller.ts
@@ -1,4 +1,4 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { prisma } from '../../database/prisma';
 import { AuthenticatedRequest } from '../middlewares/auth.middleware';
 
